e841883946182e6b1a39e0383bc0f6d4e6bd7c5c
Monineath Heng <neath@neaths-mac.local>
2022-04-21 17:29:04 -0400

    modifies tuit-schema, deletes tweet-schema and tweets-controller

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ import userController from "./controllers/user-controller.js";
 import tuitsController from "./controllers/tuits-controller.js";
 import welcomeController from "./controllers/welcome-controller.js";
 import mongoose from "mongoose";
-import tweetsController from "./controllers/tweets-controller.js";
 
 const CONNECTION_STRING = process.env.DB_PROJECT_CONNECTION_STRING
     || 'mongodb+srv://webdevfinalproject:webdevfinalpassword@cluster0.nrm2x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
@@ -17,6 +16,5 @@ app.use(express.json());
 welcomeController(app);
 userController(app);
 tuitsController(app);
-tweetsController(app);
 
 app.listen(process.env.PORT || 4000);
--- tweets/tweets-dao.js
+++
@@ -1,5 +0,0 @@
-import tweetsModel from './tweets-model.js';
-export const findAllTweets = () => tweetsModel.find();
-export const createTweet = (tweet) => tweetsModel.create(tweet);
-export const deleteTweet = (tid) => tweetsModel.deleteOne({_id: tid});
-export const updateTweet = (tid, tweet) => tweetsModel.updateOne({_id: tid}, {$set: tweet});
