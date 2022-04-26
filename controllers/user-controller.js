e01aa443345b4b1e79426f508aa3221071ed686c
Rosan <wang.ruox@northeastern.edu>
2022-04-25 23:20:53 -0400

    added bookmarks

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -86,6 +86,18 @@ const findLikedTuits = async (req, res) => {
     const tuits = [];
     for (let i = 0; i < liked_tuits.length; i++) {
         const newTuit = await tuitsDao.findTuit(liked_tuits[i]);
+        tuits.push(newTuit);x
+    }
+    res.json(tuits);
+}
+
+const findBookmarks = async (req, res) => {
+    const userId = req.params.uid;
+    const user = await usersDao.findUser(userId);
+    const bookmarks = user.bookmarks;
+    const tuits = [];
+    for (let i = 0; i < bookmarks.length; i++) {
+        const newTuit = await tuitsDao.findTuit(tuits[i]);
         tuits.push(newTuit);
     }
     res.json(tuits);
@@ -121,7 +133,8 @@ export default (app) => {
     app.post('/api/logout', logout)
     app.post('/api/profile', profile)
     app.get('/api/users/:uid', findUser);
-    app.get('/api/users/:uid/likes/tuits', findLikedTuits);
+    app.get('/api/users/:uid/likes', findLikedTuits);
+    app.get('/api/users/:uid/bookmarks', findBookmarks);
     app.get('/api/users/:uid/followers', findFollowers);
     app.get('/api/users/:uid/following', findFollowing);
     app.post('/api/users/credentials', findUserByCredentials);
